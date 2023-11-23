1da0a454f7a8f39e1eb5068ad453923d36387781
zn66665 <2602359078@qq.com>
2023-11-23 21:24:46 +0800

    new1

--- src/main.py
+++ src/main.py
@@ -1,10 +1,20 @@
 import sys
 from ui_module import SuperScurity
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
 
 #加载主窗口
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    main_window = QMainWindow()
+    ui = SuperScurity.Ui_MainWindow()
+    ui.setupUi(main_window)
+    
+    # 显示主窗口
+    main_window.show()
+    
+    # 进入应用程序主循环
+    sys.exit(app.exec_())
+
 
